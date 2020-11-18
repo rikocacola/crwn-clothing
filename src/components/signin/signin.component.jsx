d30ecf36505024d8217b6ad938cea1334c3f267e
Riko Chair Nugroho <riko.chair@gmail.com>
2020-11-19 01:28:09 +0700

    adding form-input component to signin component

---
+++ src/components/signin/signin.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import './signin.style.scss';
+
+import FormInput from '../form-input/form-input.component.jsx';
+
+class SignIn extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' });
+    };
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        type="email"
+                        name="email"
+                        value={this.state.email}
+                        label="email"
+                        handleChange={this.handleChange}
+                        required />
+                    <FormInput
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        label="password"
+                        handleChange={this.handleChange}
+                        required />
+                    <input type="submit" value="submit form" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './signin-and-signup.style.scss';
+
+import SignIn from '../../components/signin/signin.component.jsx';
+
+const SignInAndSignUpPage = () => (
+    <div className="sign-in-and-sign-up">
+        <SignIn />
+    </div>
+)
+
+export default SignInAndSignUpPage;
