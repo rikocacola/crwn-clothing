fe8c0e39cb8baa2a44d7f781a62eb87d0bb7235f
Riko Chair Nugroho <riko.chair@gmail.com>
2021-06-13 21:06:46 +0700

    update shop data, add fucntion utils add item to cart

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -51,8 +51,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ id, ...otherSectionProps }) =>
-                        <MenuItem key={id} {...otherSectionProps} />)
+                    this.state.sections.map(({id, ...otherProps}) =>
+                        <MenuItem key={id} {...otherProps} />)
                 }
             </div>
         )
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,12 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingItems = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
+
+  if(existingItems) {
+    return cartItems.map(cartItem => 
+      cartItem.id === cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1}
+      : cartItem
+      )
+  } else {
+    return [...cartItems, {...cartItemToAdd, quantity :  1}]
+  }
+}
